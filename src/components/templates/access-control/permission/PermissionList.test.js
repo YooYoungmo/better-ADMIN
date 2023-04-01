b8c52b3387a8058d9113f7d02166965a014bbae5
Young-mo Yoo <gigamadness@gmail.com>
2023-04-01 09:01:58 +0900

    돌아가는 테스트 코드를 만들고 TODO를 만듬

--- src/components/templates/access-control/permission/PermissionList.test.js
+++ src/components/templates/access-control/permission/PermissionList.test.js
@@ -6,7 +6,7 @@ import AccessControlService from '../../../../services/access.control.service';
 
 jest.mock('../../../../services/access.control.service');
 
-test('Button Rendering', async () => {
+test('테이블 렌더링', async () => {
   // given
   const resp = {
     data: {
@@ -43,5 +43,55 @@ test('Button Rendering', async () => {
   // then
   const permissionTable = screen.getByTestId('permission-table');
   expect(permissionTable).toBeInTheDocument();
-  expect(permissionTable).toHaveTextContent('재고 접근 권한');
+
+  // 테이블 헤더 검증
+  const header = permissionTable.querySelector('thead > tr');
+  const headerColumns = header.querySelectorAll('th');
+  expect(headerColumns).toHaveLength(4);
+  expect(headerColumns[0]).toHaveTextContent('유형');
+  expect(headerColumns[1]).toHaveTextContent('권한 이름');
+  expect(headerColumns[2]).toHaveTextContent('설명');
+  expect(headerColumns[3]).toHaveTextContent('Action');
+
+  // 테이블 바디 검증
+  const bodyRows = permissionTable.querySelectorAll('tbody > tr');
+  expect(bodyRows).toHaveLength(1);
+  const bodyRowColumns = bodyRows[0].querySelectorAll('td');
+  expect(bodyRowColumns).toHaveLength(4);
+  expect(bodyRowColumns[0]).toHaveTextContent('사용자정의');
+  expect(bodyRowColumns[1]).toHaveTextContent('ACCESS_STOCK');
+  expect(bodyRowColumns[2]).toHaveTextContent('재고 접근 권한');
 });
+
+
+// describe('테이블 랜더링', () => {
+//   test('url parameter 가 없을 때 기본적으로 1페이지 10개를 렌더링 한다.', async () => {
+//
+//   });
+//
+//   test('url parameter에 page 2와 size 2를 명기한 경우에 2페이지와 2개씩 렌다링 한다.', async () => {
+//
+//   });
+//
+//   test('권한 유형이 사용자 정의일 때 Action 버튼이 노출 한다.', async () => {
+//
+//   });
+//
+//   test('권한 유형이 사전 정의일 때 Action 버튼이 노출하지 않는다.', async () => {
+//
+//   });
+// });
+//
+// describe('페이지 Action', () => {
+//   test('권한 생성 버튼을 누르면 권한 생성 페이지로 이동한다.', async () => {
+//
+//   });
+//
+//   test('테이블에 권한 수정 버튼을 누르면 수정 페이지로 이동한다.', async () => {
+//
+//   });
+//
+//   test('테이블에 권한 삭제 버튼을 누르면 삭제 확인 창을 띄우고 삭제 한다.', async () => {
+//
+//   });
+// });
